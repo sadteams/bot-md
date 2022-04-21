6df8b14ec7cbc1df5329214f4f254279b9e251db
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2022-04-21 17:23:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Readline = require('readline')
 const yargs = require('yargs/yargs')
 const rl = Readline.createInterface(process.stdin, process.stdout)
 
-CFonts.say('Family-Bot', {
+CFonts.say('Raxcel-Bot', {
   colors: ['#f2aa4c'],
   font: 'block',
   align: 'center',
