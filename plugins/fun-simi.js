7afe75f80d6e82cad1e1ba8838ef47bf79668b24
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2022-06-07 20:52:03 +0700

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api-sv2.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "id" }, ''))
+  let res = await fetch(global.API('https://api-sv2.simsimi.net', '/v2/', { text: text, lc: "id" }, ''))
   let json = await res.json()
   if (json.success) m.reply(json.success)
   else throw json
