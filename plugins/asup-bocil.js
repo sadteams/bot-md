158503de8489fbc99777e273fb5437fc47048b31
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-08-14 11:26:19 +0800

    bocil

---
+++ plugins/asup-bocil.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn, usedPrefix, command }) => {
+  let res = await fetch('https://raw.githubusercontent.com/binjaicity/warga62/master/bocil.json')
+  //if (!res.ok) throw await res.json()
+  let asup = await res.json()
+  let json = asup[Math.floor(Math.random() * asup.length)]
+  //if (!json.url) throw 'Error!'
+  conn.sendButtonVid(m.chat, json.url, command, wm, command, `${usedPrefix} command`, m)
+}
+handler.help = ['bocil']
+handler.tags = ['asupan']
+handler.command = /^(bocil)$/i
+//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+module.exports = handler
