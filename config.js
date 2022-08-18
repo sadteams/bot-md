79565399d3f8ae673c41ef8b029d7bb750afebf3
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2022-08-18 18:24:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 let fs = require('fs')
+global.data = JSON.parse(fs.readFileSync('./data.json'))
 global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Put your number to /src/owner.json
 global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some help?
-global.thumbt = global.img
-global.fsx = 999999999999
-global.kontak = [
-['62895336282144', '𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷', true],
-['6282140373458', 'Raxcel-MD ⁴⁰⁴-ﾉ [BOT MD]', true]
-]
+
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -33,58 +29,13 @@ global.APIKeys = { // APIKey Here
   'https://hardianto.xyz': 'hardianto',
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
-
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷'
-global.linkgc = 'https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP'
-global.linkig = 'https://www.instagram.com/the.sad.boy01'
-global.deslink = ''
-global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@the.sad.boy01'
-
-global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
-global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
-
-//========Url Template Buttons==========//
-global.dtu = '𝑭𝒐𝒍𝒍𝒐𝒘'
-global.urlnya = "https://www.instagram.com/the.sad.boy01"
-
-//============= callButtons ============//
-global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
-
-//========== Tampilan Bot =============//
-global.sa = '╭─'
-global.gx = '│✇'
-global.gy = '│•'
-global.gz = '│'
-global.sb = '╰────࿐'
-global.kki = '「'
-global.kka = '」'
-global.zt = '*'
-global.zc = ''
-
-global.thumbnailUrl = [
-  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
-  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
-  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
-  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
-  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
-]
-
-//============= Games ================//
-global.benar = '_*Benar✅*_'
-global.salah = '_*Salah❌*_'
-global.dikit = "dikit lagi, semangat ya :')"
+global.packname = data.namabot
+global.author = data.owner
+global.wm = data.namabot + ' ' + data.owner
 
 global.multiplier = 100 // The higher, The harder levelup
 
 //=========== Requirements ==========//
-
-global.baileys = require('@adiwajshing/baileys')
-global.fs = require('fs')
-global.data = JSON.parse(fs.readFileSync('./data.json'))
-global.fetch = require('node-fetch')
-global.bochil = require('@bochilteam/scraper')
 
 global.rpg = {
   emoticon(string) {
@@ -99,7 +50,7 @@ global.rpg = {
       diamond: '💎',
       common: '📦',
       uncommon: '🎁',
-      mythic: '🗳️',
+      mythi: '🗳️',
       legendary: '🗃️',
       pet: '🎁',
       sampah: '🗑',
