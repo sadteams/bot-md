a735ae4764bfc099b9f8f1e560a7057ddbcadae7
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-09-12 05:51:07 +0800

    fix nhentaipdf

--- plugins/nsfw-nhentaipdf.js
+++ plugins/nsfw-nhentaipdf.js
@@ -1,4 +1,4 @@
-let { get } = require('axios')
+/*let { get } = require('axios')
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...kode nya mana?'
   m.reply('Searching....')
@@ -18,4 +18,26 @@ async function getBuffer(url) {
 k = await require('node-fetch')(url)
 a = await k.buffer()
 return a 
+}*/
+
+
+
+let fetch = require('node-fetch')
+let { extractImageThumb } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, args }) => {
+	let code = (args[0] || '').replace(/\D/g, '')
+	if (!code) throw 'Input code' 
+	await m.reply('_In progress, please wait..._')
+	let lol = await global.API('lol', `/api/nhentai/${code}`, {}, 'apikey')	
+	let lolhum = await global.API('lol', `/api/nhentaipdf/${code}`, {}, 'apikey')	
+        let buffer = await (await fetch(v.result.image[0])).buffer()
+	let jpegThumbnail = await extractImageThumb(buffer)	
+	conn.sendMessage(m.chat, { document: { url: i.result }, jpegThumbnail, fileName: json.result.title_romaji + '.pdf', mimetype: 'application/pdf' }, { quoted: m })
 }
+handler.help = handler.alias = ['nhpdf']
+handler.tags = ['dewasa']
+handler.command = /^(nhentai)$/i
+handler.limit = 1
+handler.nsfw = true
+module.exports = handler
