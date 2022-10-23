87c16edfc78e7a4f53dd7c925d2b7a4975b8bf00
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-10-24 05:13:47 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,4 +1,4 @@
-let fetch = require('node-fetch')
+/*let fetch = require('node-fetch')
 let axios = require('axios')
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
@@ -25,4 +25,111 @@ handler.tags = ['downloader']
 
 handler.command = /^((tt|tiktok)?(dl)?)$/i
 
+module.exports = handler*/
+
+/*Thank To
+Xfarr (https://github.com/xfar05)
+Nevt (gw) (https://github.com/NevtBotz)
+Mau req fitur ?
+Chat wa.me/6281805450249
+Atau Di issue
+Sebisa mungkin Saiya buatkan fitur nya
+*/
+
+const x = require('xfarr-api')
+const bocil = require('@bochilteam/scraper')
+const fetch = require('node-fetch')
+const { toAudio, toPTT } = require('../lib/converter')
+let handler = async (m, { conn, command, args, text }) => {
+if (command == 'tiktok') {
+if (!args[0]) throw 'Used Example tiktok https://vt.tiktok.com/ZSRGkNj6E/'
+let a = await x.downloader.tiktok(args[0])
+let { title, thumbnail } = await a
+let c = await thumbnail.replace('https:','')
+conn.send3ButtonDoc(m.chat, title, 'Silahkan pencet button di bawah ini untuk download tiktok', 'Watermak', `.tiktokwm ${text}`, 'No Watermak', `.tiktoknowm ${text}`, 'Audio', `.tiktokaudio ${text}`, m, {
+  contextInfo: {
+    externalAdReply :{
+    mediaUrl: linkig,
+    mediaType: 1,
+    description: deslink, 
+    title: titlink,
+    body: wm,
+    thumbnail: await(await fetch(c)).buffer(),
+    renderLargerThumbnail: true,
+    sourceUrl: linkgc
+                             }}
+                           })
+}
+if (command == 'tiktokwm') {
+if (!args[0]) throw 'Used Example tiktokwm https://vt.tiktok.com/ZSRGkNj6E/'
+let a = await x.downloader.tiktok(args[0])
+let { title, media } = await a
+let { quality, extension, size, formattedSize } = await media[0]
+let buf = await (await fetch(media[0].url)).buffer()
+let tek = `*Title:* ${title}\n*Quality:* ${quality}\n*Extension:* ${extension}\n*Size:* ${size}\n*FormattedSize:* ${formattedSize}`
+await conn.sendFile(m.chat, buf, null, tek, m)
+}
+if (command == 'tiktoknowm') {
+if (!args[0]) throw 'Used Example tiktoknowm https://vt.tiktok.com/ZSRGkNj6E/'
+try {
+var a = await x.downloader.tiktok(args[0])
+var { title, media } = await a
+var { quality, extension, size, formattedSize } = await media[1]
+var buf = await (await fetch(media[1].url)).buffer()
+var tek = `*Title:* ${title}\n*Quality:* ${quality}\n*Extension:* ${extension}\n*Size:* ${size}\n*FormattedSize:* ${formattedSize}`
+await conn.sendFile(m.chat, buf, null, tek, m)
+ } catch {
+try { 
+var l = await bocil.tiktokdl(args[0])
+var { author: { nickname }, video, description } = await l
+var url = video.no_watermark || video.no_watermark2
+await conn.sendFile(m.chat, url, 'tiktok.mp4', `
+*Nickname:* ${nickname}
+*Description:* ${description}
+`.trim(), m)
+ } catch {
+ try { 
+var l = await bocil.tiktokdlv2(args[0])
+var { author: { nickname }, video, description } = await l
+var url = video.no_watermark || video.no_watermark2
+await conn.sendFile(m.chat, url, 'tiktok.mp4', `
+*Nickname:* ${nickname}
+`.trim(), m)
+ } catch {
+      }
+    } 
+ }}
+if (command == 'tiktokaudio') {
+if (!args[0]) throw 'Used Example tiktokaudio https://vt.tiktok.com/ZSRGkNj6E/'
+try {
+    var { author: { nickname }, video, description } = await bocil.tiktokdl(args[0])
+    var url = video.no_watermark || video.no_watermark2
+    var a = await(await fetch(url)).buffer()
+    var au = await toAudio(a, 'mp4')
+    var vn = await toPTT(a, 'mp4')
+    await conn.sendFile(m.chat, au.data, 'tiktok.mp3', m, false, { mimetype: 'audio/mp4' })
+  } catch {
+try {
+var a = await x.downloader.tiktok('https://vt.tiktok.com/ZSRGkNj6E/')
+var { title, thumbnail, media } = a
+var b = await media[2].url
+conn.sendFile(m.chat, b, 'error.mp3', '', m, false, { mimetype: 'audio/mp4' })
+} catch {
+  }
+ }
+}}
+handler.command = [
+'tiktok',
+'tiktokwm',
+'tiktoknowm',
+'tiktokaudio'
+]
+handler.help = [
+'tiktok <link>',
+'tiktokwm <link>',
+'tiktoknowm <link>',
+'tiktokaudio <link>'
+]
+handler.tags = ['downloader']
+handler.limit = true
 module.exports = handler
