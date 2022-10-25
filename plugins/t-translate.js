0fe0f1020e7580df74b74d23d2a2c0adf220dc5b
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-10-26 02:59:20 +0700

    Simple fix

--- plugins/t-translate.js
+++ plugins/t-translate.js
@@ -1,6 +1,6 @@
 const translate = require('translate-google-api')
 const defaultLang = 'en'
-const tld = 'cn'
+//const tld = 'cn'
 
 let handler = async (m, { args, usedPrefix, command }) => {
     let err = `
@@ -22,12 +22,12 @@ Daftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages
     let result
     try {
         result = await translate(`${text}`, {
-            tld,
+            //tld,
             to: lang,
         })
     } catch (e) {
         result = await translate(`${text}`, {
-            tld,
+            //tld,
             to: defaultLang,
         })
         throw err
