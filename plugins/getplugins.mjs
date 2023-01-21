c4290a668e1f631d330a41e64afc517a9fcc3fd9
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2023-01-21 11:52:24 +0700

    Update getplugins.mjs

--- plugins/getplugins.mjs
+++ plugins/getplugins.mjs
@@ -4,7 +4,7 @@ let handler = async (m, { usedPrefix, command, text }) => {
     let ar1 = ar.map(v => v.replace('.js', ''))
     if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} creator`
     if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
-    m.reply(readFileSync('./plugins/' + text + '.js', 'utf-8'))
+    m.reply(readFileSync('./plugins/' + text + '.mjs', 'utf-8'))
 }
 handler.help = ['getplugin'].map(v => v + ' <teks>')
 handler.tags = ['owner']
