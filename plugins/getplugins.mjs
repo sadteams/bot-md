7b026fd4fde2e4df6d530361aa9a0302e722e93e
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2023-01-21 11:54:07 +0700

    Update getplugins.mjs

--- plugins/getplugins.mjs
+++ plugins/getplugins.mjs
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs'
 let handler = async (m, { usedPrefix, command, text }) => {
     let ar = Object.keys(plugins)
-    let ar1 = ar.map(v => v.replace('.js', ''))
+    let ar1 = ar.map(v => v.replace('.mjs', ''))
     if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} creator`
     if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
     m.reply(readFileSync('./plugins/' + text + '.mjs', 'utf-8'))
