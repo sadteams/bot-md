930f52ed1bfcb1d10d25f9c1d0caf93a87218adb
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2023-01-21 11:51:39 +0700

    Update savefileplugins.mjs

--- plugins/savefileplugins.mjs
+++ plugins/savefileplugins.mjs
@@ -3,7 +3,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
 
     if (!text) throw `where is the path?\n\nexample:\n${usedPrefix + command} plugins/menu.js`
     if (!m.quoted.text) throw `reply code`
-    let path = `plugins/${text}.js`
+    let path = `plugins/${text}.mjs`
     await writeFileSync(path, m.quoted.text)
 
     m.reply(`Saved ${path} to file!`)
